e960438e97cd55e270b16ef13cec3379891f5cc3
psi <psi@7io.org>
2011-04-28 23:15:23 +0900

    first commit

---
+++ shobo_cal.rb
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+
+=begin
+    ShoboCal_Renamer
+    Copyright (C) 2011 PSI
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
+require 'optparse'
+require 'open-uri'
+require 'rexml/document'
+require 'scanf'
+
+SHOBOCAL_DB_URL="http://cal.syoboi.jp/db.php?Command=TitleLookup&TID=%d&Fields=Title,SubTitles"
+
+class Title
+	def initialize(title_number)
+		@SubTitles = Array.new;
+		uri = URI.parse(sprintf(SHOBOCAL_DB_URL,title_number));
+		@Title = sub_titles = REXML::Document.new(uri.read).elements['TitleLookupResponse/TitleItems/TitleItem/Title'].get_text.to_s;
+		sub_titles = REXML::Document.new(uri.read).elements['TitleLookupResponse/TitleItems/TitleItem/SubTitles'].get_text.to_s;
+		sub_titles.each_line{|line|
+			elm = line.scanf("*%d*%s")
+			#FIXME: このやばいくらいに漂うバッドノウハウ感
+			# ファイル名に使用できない文字を全角に変換しています。
+			@SubTitles[elm[0]] = elm[1].tr('a-zA-Z!?:;/\|,*"><','ａ-ｚＡ-Ｚ！？：；／￥｜，＊')
+		}
+		@SubTitles.freeze
+	end
+	
+	def getNumberOfSubTitles()
+		return @SubTitles.size
+	end
+	def getTitle
+		return @Title
+	end
+	def getSubTitle(num)
+		return @SubTitles[num]
+	end
+end
+
+class TitleResolver
+	def initialize()
+		@TitleBank = Hash.new
+	end
+	def check(title)
+		unless @TitleBank.has_key? title
+			@TitleBank[title] = Title.new(title)
+		end
+	end
+	protected :check
+	def getNumberOfSubTitles(title)
+		check(title)
+		return @TitleBank[title].getNumberOfSubTitles
+	end
+	def getTitleSet(title, num)
+		check(title)
+		return @TitleBank[title].getTitle(), @TitleBank[title].getSubTitle(num)
+	end
+	def getTitle(title)
+		check(title)
+		return @TitleBank[title].getTitle(num)
+	end
+	def getSubTitle(title, num)
+		check(title)
+		return @TitleBank[title].getSubTitle(num)
+	end
+	def self.getInstance()
+		unless defined?(@@obj)
+			@@obj = TitleResolver.new
+		end
+		return @@obj;
+	end
+end
+
+class Formatter
+	FormatPattern = /%(.*?)%/
+	ReplacePattern = {
+		"ext" => "(.+)",
+	}
+	def initialize(format_str)
+		@idx_hash = Hash.new
+		cnt = 0
+		format_str_regexp = "^"+Regexp.escape(format_str)+"$"
+		pat_str = format_str_regexp.gsub(FormatPattern){|str|
+			@idx_hash[$1] = cnt
+			cnt += 1
+			if ReplacePattern.key? $1
+				ReplacePattern[$1]
+			else
+				"(\\d*)"
+			end
+		}
+		@pattern = Regexp.compile(pat_str)
+		@for_sprinrf = format_str.gsub(FormatPattern, '%s')
+	end
+	def match(str)
+		matched = @pattern.match(str)
+		unless matched
+			return nil
+		end
+		ret = Hash.new
+		@idx_hash.each{|key, value|
+			ret[key] = matched[value+1] #最初の一個はマッチ全体。
+		}
+		return ret
+	end
+	def format(hash)
+		format_array = Array.new(@idx_hash.size, "")
+		hash.each{|key, val|
+			format_array[@idx_hash[key]] = val if @idx_hash.key? key
+		}
+		return sprintf(@for_sprinrf, *format_array);
+	end
+end
+
+def getDefaultRunInfo
+	info = Hash.new
+	info[:in_format] = Formatter.new("%tid%-%stid%-%date%-%time%.%ext%");
+	info[:out_format] = Formatter.new("%title% 第%stid%話 %subtitle% (%date%-%time%).%ext%");
+	info[:dry_run] = false;
+	info[:recursive] = false;
+	info[:dont_ask] = false;
+	info[:except_pettern] = Array.new
+	return info
+end
+
+def rename(info, filename)
+	basename = File::basename(filename)
+	info[:except_pettern].each(){|pat|
+		if pat == basename
+			return
+		end
+	}
+	file_info = info[:in_format].match(basename)
+	unless file_info
+		return
+	end
+
+	tid = file_info["tid"].to_i;
+	stid = file_info["stid"].to_i
+	title, subtitle = TitleResolver.getInstance.getTitleSet(tid,stid)
+	digits_of_stid = 1+Math::log10(TitleResolver.getInstance.getNumberOfSubTitles(tid)).to_i #最大の話数は何桁になる？
+	file_info["stid"] = file_info["stid"].rjust([2,digits_of_stid].max,"0"); #話数のパディング←Vistaだとしなくてもうまくソート出来た気がするけど…
+
+	renamed_title = info[:out_format].format( file_info.merge({"title" => title, "subtitle" => subtitle}))
+	renamed_path = File::join(File::dirname(filename),renamed_title)
+	puts "\"#{filename}\" => \"#{renamed_path}\""
+	unless info[:dry_run]
+		if info[:dont_ask] || !FileTest.exist?(renamed_path)
+			File::rename(filename, renamed_path)
+		else
+			accepted = false
+			while !accepted
+				print "\"#{renamed_path}\" overwrite? [y/n]: "
+				$stdout.flush()
+				buf = ($stdin.gets).strip
+				accepted  = ( buf == "n" || buf == "y" )
+			end
+			if buf == "y"
+				File::rename(filename, renamed_path)
+			end
+		end
+	end
+end
+
+def enum(info,targets, depth)
+	targets.each(){|target|
+		filelist = Dir.glob(target);
+		filelist.each{|filename|
+			type = File::ftype(filename);
+			if type == "directory"
+				if info[:recursive]
+					enum(info, [filename+"/*"],depth+1)
+				elsif depth == 0 && FileTest::directory?(target)#フォルダ名を直接指定している場合、ということ。
+					enum(info, [filename+"/*"],depth+1)
+				end
+			elsif  type == "file"
+				rename(info, filename);
+			end
+		}
+	}
+#	title_resolver = TitleResolver.new()
+#	puts title_resolver.getSubTitle(2099, 3)
+end
+
+Version = "1.0 (2011/04/28)"
+Banner = <<EOF
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ShoboCal_Renamer #{Version}
+                   written by PSI ( http://ledyba.ddo.jp/ )
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+usage: ruby #{$0} [options] files or directories
+EOF
+
+def main(argv = ARGV)
+	opts = OptionParser.new(Banner)
+	opts.program_name = "ShoboCal_Renamer"
+	info = getDefaultRunInfo();
+	opts.on("-i","--in_format FORMAT",String, /.*/, "入力ファイルのフォーマットを指定してください。"){|v|
+		info[:in_format] = Formatter.new(v);
+	}
+	opts.on("-o","--out_format FORMAT",String , /.*/, "出力ファイルのフォーマットを指定してください。"){|v|
+		info[:out_format] = Formatter.new(v);
+	}
+	opts.on("-d", "--dry-run", nil, "ファイル名を実際には書き換えません。") {|v|
+		info[:dry_run] = true;
+	}
+	opts.on("-r", "--recursive", nil, "再帰的に実行します。") {|v|
+		info[:recursive] = true;
+	}
+	opts.on("-e", "--except FILENAME", String, /.*/, "指定したファイルは除外します。") {|v|
+		info[:except_pettern].push(v);
+	}
+	opts.on("-y", TrueClass, nil,"上書きが必要な際に、その旨を聞きません。") {|v|
+		info[:dont_ask] = true
+	}
+	opts.on_tail("-h", "--help", "このメッセージを表示します") {
+	  puts opts.help
+	  exit
+	}
+	opts.on_tail("--version", "バージョンを表示します。") {
+	  puts opts.ver
+	  exit
+	}
+
+	opts.parse!(argv)
+	enum(info,argv, 0);
+end
+
+main
